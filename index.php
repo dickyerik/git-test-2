5cb0c8123348d0052b29b3c7c676e4f95cd3137d
dickyerik <dickyerik@gmail.com>
2023-03-03 17:10:33 +0700

    menambahkan paragraf

--- index.php
+++ index.php
@@ -10,6 +10,7 @@
 
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus deleniti debitis aut molestiae quod sed delectus soluta dignissimos? Voluptates hic, quia quos odio velit veniam nisi esse nesciunt qui id.</p>
 </body>
 
 </html>
