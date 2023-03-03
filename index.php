eb3f6f4c2a0c786bbd6b489fd9c455d727be2825
dickyerik <dickyerik@gmail.com>
2023-03-03 17:18:06 +0700

    menambahkan li

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloribus deleniti debitis aut molestiae quod sed delectus soluta dignissimos? Voluptates hic, quia quos odio velit veniam nisi esse nesciunt qui id.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+        <li>empat</li>
+    </ul>
 </body>
 
 </html>
